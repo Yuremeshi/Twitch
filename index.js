cbc69de937b9c06d93c3f3c08ca77db52cd5dff1
Yuremeshi <yuremeshi.code@gmail.com>
2023-08-01 23:33:11 -0400

    Bunch of changes

--- index.js
+++ index.js
@@ -11,22 +11,41 @@ window.addEventListener('load', function() {
 })
 */
 let open = 0;
+let dropdownClass;
 
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelectorAll(".dropdown-button").forEach(btn => {
         btn.addEventListener("click", e=> {
             document.querySelectorAll(".dropdown-content").forEach(div => div.classList.remove("show"));
 
-            e.target.closest(".dropdown").querySelector(".dropdown-content").classList.add("show");
+            e.target.closest(".dropdown").querySelector(".dropdown-content").classList.toggle("show");
+            console.log(e.target);
+
+
         })
     })
 
     document.addEventListener("click", e => {
-        if (e.target.classList.contains("dropdown-button") && open == 0) {
-            open = 1;
+        if (e.target.classList.contains("dropdown-button")) {
+            console.log("Hello3");
             return;
         }
+
+        if (e.target.classList.contains("search-bar-message")) return;
+
+        if (e.target.closest(".dropdown")) {
+            if (e.target.classList.contains("close")) {
+                document.querySelectorAll('.dropdown-content').forEach(div => div.classList.remove('show'));
+                console.log("Hello4")
+            }
+            if (e.target.closest(".dropdown").querySelector(".dropdown-content").classList.contains("show")) return;
+        }
+
         document.querySelectorAll('.dropdown-content').forEach(div => div.classList.remove('show'));
-        open = 0;
+        
+
+        console.log(e.target)
+        console.log(e.target.closest(".dropdown-content"))
+        console.log("Hello5");
     })
 })
